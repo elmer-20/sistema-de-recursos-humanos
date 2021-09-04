da8805efb8f8341fffebbb3aa555cf5b6e7211dc
elmer-20 <elmerjk.20@gmail.com>
2021-09-03 19:19:00 -0500

    RRHH
    recursos humanos

---
+++ resources/assets/js/Datatable.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <md-table>
+
+                <md-table-head class="table-header text-center" v-for="column in columns" :key="column.name" @click="$emit('sort', column.name)">
+                    {{column.label}}
+                </md-table-head>
+
+        <slot></slot>
+    </md-table>
+</div>
+</template>
+
+<script>
+    export default {
+        props: ['columns', 'sortKey', 'sortOrders']
+    };
+</script>
+
+<style lang="scss" scoped>
+  .table-header {
+    padding-top:10px;
+    padding-bottom:10px;
+    background-color:purple;
+    color: white !important;
+  }
+
+</style>
---
+++ resources/assets/js/Pagination.vue
@@ -0,0 +1,14 @@
+<template>
+<div id="paginacion">
+    <span>{{pagination.from}} - {{pagination.to}} de {{filtered.length}}</span>
+    <md-button class="md-icon-button md-dense pull-right md-primary" @click="$emit('next');"><md-icon>keyboard_arrow_right</md-icon></md-button>
+    <md-button class="md-icon-button md-dense pull-right md-primary" @click="$emit('prev');"><md-icon>keyboard_arrow_left</md-icon></md-button>
+</div>
+</template>
+
+<script>
+    export default {
+        props: ['pagination', 'client', 'filtered']
+    };
+</script>
+
